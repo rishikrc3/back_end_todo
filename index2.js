f7564b6b7d7b063f069629f6ebef93d435743910
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-18 17:14:40 +0530

    put,get, delete

---
+++ index2.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const fs = require("fs");
+const port = 3000;
+app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => {
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    res.json(JSON.parse(data));
+  });
+});
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    description: req.body.description,
+  };
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    todos.push(newTodo);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err, data) => {
+      if (err) throw err;
+      res.status(201).json(newTodo);
+    });
+  });
+});
+app.delete("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos = removeAtIndex(todos, todoIndex);
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if (err) throw err;
+        res.status(200).send();
+      });
+    }
+  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
