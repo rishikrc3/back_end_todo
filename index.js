aa7a93acd656f05c5c3169d4a786ea7f3b654612
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-18 10:08:47 +0530

    get_post

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+let todos = [];
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
 });
 
 app.listen(port, () => {
